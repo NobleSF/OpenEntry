3114588270ca973fb36c493200f6edbe2b784377
Tom Counsell <contact@tomcounsell.com>
2015-07-10 16:48:28 -0400

    model behaviors

---
+++ apps/common/behaviors/expirable.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Expirable(models.Model):
+  expired_at    = models.DateTimeField(null=True)
+
+  @property
+  def is_expired(self):
+    from django.utils.timezone import now
+    return True if self.expired_at and self.expired_at < now() else False
+
+  class Meta:
+    abstract = True
