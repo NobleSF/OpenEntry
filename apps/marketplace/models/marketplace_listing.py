eae85192dd2aa1aad11dc8236adb468b84e0a07d
Tom Counsell <contact@tomcounsell.com>
2015-07-31 00:51:53 +0700

    marketplace listing model

---
+++ apps/marketplace/models/marketplace_listing.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django_extensions.db.fields.json import JSONField
+from apps.common.models.base_listing import BaseListing
+
+
+class MarketplaceListing(BaseListing):
+  """
+  A unique listing belonging to a marketplace.
+
+  Inherites base model fields: product, currency, retail_price, sale_price, wholesale_price
+  Inherits mixin fields: created_at, modified_at, published_at, unpublished_at, valid_at, expired_at, slug
+  """
+  marketplace       = models.ForeignKey('marketplace.Marketplace')
+
+
+  # MODEL PROPERTIES
+
+  # MODEL FUNCTIONS
+
+
+  class Meta:
+    verbose_name_plural = 'marketplace listings'
+    ordering = ['-created_at',]
