41d1463a248dd82768061fad5364df6e26befc42
Tom Counsell <contact@tomcounsell.com>
2015-08-11 16:26:35 +0700

    install django restframework

--- settings/urls.py
+++ settings/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+  url(r'^api-browsable/', include('rest_framework.urls', namespace='rest_framework'))
 )
