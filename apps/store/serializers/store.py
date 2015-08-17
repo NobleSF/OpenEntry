fd13749834e750e07e99502b664f5d9df7bc1764
Tom Counsell <contact@tomcounsell.com>
2015-08-17 16:40:30 +0700

    fill out Store serializer

--- apps/store/serializers/store.py
+++ apps/store/serializers/store.py
@@ -25,6 +25,30 @@ from apps.store.models import Store
 #     return instance
 
 class StoreSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-      model = Store
-      fields = ('url', 'name', 'subdomain', 'domain', 'products')
+
+  #Timestampable
+  created_at        = serializers.DateTimeField(read_only=True)
+  modified_at       = serializers.DateTimeField(read_only=True)
+
+  #Publishable
+  published_at      = serializers.DateTimeField(required=False)
+  unpublished_at    = serializers.DateTimeField(required=False)
+  is_published      = serializers.BooleanField(read_only=True)
+
+  #Permalinkable
+  slug              = serializers.SlugField(required=False)
+  get_absolute_url  = serializers.CharField(read_only=True)
+
+  class Meta:
+    model = Store
+    fields = ('url',
+              'name', 'subdomain', 'domain',
+              'default_currency',
+
+              'created_at', 'modified_at',
+              'published_at', 'unpublished_at', 'is_published',
+              'slug', 'get_absolute_url',
+
+              #related GET only
+              'listings', 'categories', 'products',
+              )
